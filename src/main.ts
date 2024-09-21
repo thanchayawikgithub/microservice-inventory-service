feff54d46564fa4be295a8be2690a49d8431bdca
thanchayawik <thanchayawik@gmail.com>
2024-09-22 00:14:19 +0700

    add rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const inventoryQueue =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'inventory_queue',
+        noAck: false,
+        queueOptions: {
+          durable: true,
+        },
+      },
+    });
+
+  await inventoryQueue.listen();
 }
 bootstrap();
